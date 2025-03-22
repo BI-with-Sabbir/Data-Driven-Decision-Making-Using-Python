02040de37d7385cee38c7d29343a5f00740468d6
Sabbir Hossain Rossi <shabbir.biforbusiness@gmail.com>
2025-03-22 06:18:24 +0600

    Update XGboost.py

--- Hourly Time Series Forecasting using XGBoost/XGboost.py
+++ Hourly Time Series Forecasting using XGBoost/XGboost.py
@@ -1,1 +1,8 @@
-
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns
+import matplotlib.pyplot as plt
+import xgboost as xgb
+from xgboost import plot_importance, plot_tree
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+plt.style.use('fivethirtyeight')
